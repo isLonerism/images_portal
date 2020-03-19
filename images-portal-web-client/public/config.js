db002c5987e174a537dcbcdd2f86ec98fb82b207
Vlad <vlad@localhost.localdomain>
2020-03-19 15:29:39 -0400

    feat(access token retrieval through openshift authentication)

--- images-portal-web-client/public/config.js
+++ images-portal-web-client/public/config.js
@@ -1,12 +1,15 @@
 window.ENV = {
-    "S3_BUCKET_NAME":"testbucket",
-    "S3_ACCESS_KEY_ID":"ordavid55",
-    "S3_SECRET_ACCESS_KEY":"ordavid55",
-    "S3_ENDPOINT":"http://172.17.0.2:9000",
-    "IMAGE_PUSH_ROUTE":"https://images-portal-grpc-client.router.myproject.svc.cluster.local/push",
-    "IMAGE_LOAD_ROUTE":"https://images-portal-grpc-client.router.myproject.svc.cluster.local/load",
-    "OSFT_REGISTRY_PREFIX":"docker-registry.default.svc:5000",
+    "S3_BUCKET_NAME": "testbucket",
+    "S3_ACCESS_KEY_ID": "ordavid55",
+    "S3_SECRET_ACCESS_KEY": "ordavid55",
+    "S3_ENDPOINT": "http://172.17.0.2:9000",
     "S3_UPLOAD_TIMEOUT_MS": "180000",
+    "IMAGE_PUSH_ROUTE": "https://images-portal-grpc-client.router.myproject.svc.cluster.local/push",
+    "IMAGE_LOAD_ROUTE": "https://images-portal-grpc-client.router.myproject.svc.cluster.local/load",
+    "OSFT_REGISTRY_PREFIX": "docker-registry.default.svc:5000",
     "LOAD_TIMEOUT_MS": "180000",
-    "PUSH_TIMEOUT_MS": "180000"
+    "PUSH_TIMEOUT_MS": "180000",
+    "OAUTH_OPENSHIFT_ROUTE": "https://192.168.42.116",
+    "OAUTH_CLIENT_ID": "openshift-images-portal"
+/*  "USER_ACCESS_TOKEN" is available upon authentication */
 }
