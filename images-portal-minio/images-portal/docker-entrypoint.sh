a371de0eabd91204ff329a4750859a8928b3f4ce
isLonerism <isLonerism>
2020-08-24 20:03:47 +0300

    fix(envsubst to a temp file, then overwrite)

--- images-portal-minio/images-portal/docker-entrypoint.sh
+++ images-portal-minio/images-portal/docker-entrypoint.sh
@@ -18,7 +18,8 @@ echo -n "Setting up '$BUCKET_NAME' bucket and users... "
 
 # evaluate lifecycle days within bucket policy
 cd $(dirname "$0")
-envsubst < data/.minio.sys/buckets/BUCKET_NAME/lifecycle.xml > data/.minio.sys/buckets/BUCKET_NAME/lifecycle.xml
+envsubst < data/.minio.sys/buckets/BUCKET_NAME/lifecycle.xml > /tmp/lifecycle.xml
+mv -f /tmp/lifecycle.xml data/.minio.sys/buckets/BUCKET_NAME/lifecycle.xml
 
 # change placeholder bucket name to actual bucket name, create bucket folder
 mv data/.minio.sys/buckets/BUCKET_NAME data/.minio.sys/buckets/$BUCKET_NAME
@@ -31,7 +32,8 @@ cd data/.minio.sys/config/iam
 for user in WEB_CLIENT GRPC_SERVER;
 do
 	# evaluate credentials within identity files
-	envsubst < users/$user/identity.json > users/$user/identity.json
+	envsubst < users/$user/identity.json > /tmp/identity.json
+	mv -f /tmp/identity.json users/$user/identity.json
 
 	# change placeholder user directory names to actual user IDs
 	mv users/$user users/$(eval "echo \$${user}_S3_ACCESS_KEY")
@@ -43,7 +45,8 @@ done
 # evaluate bucket name within canned policies for users
 for policy in images-portal-grpc-server-policy images-portal-web-client-policy;
 do
-	envsubst < policies/$policy/policy.json > policies/$policy/policy.json
+	envsubst < policies/$policy/policy.json > /tmp/policy.json
+	mv -f /tmp/policy.json policies/$policy/policy.json
 done
 cd - > /dev/null
 
